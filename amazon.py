9669872774cc16237340f4fba5c73c7d543aa2d9
Mrinal Sinha <themrinalsinha@gmail.com>
2018-11-26 20:04:40 +0530

    searching and extracting info based on ASIN info

--- amazon.py
+++ amazon.py
@@ -17,3 +17,14 @@ class AmazonScraper(object):
             self.html = fromstring(response.text)
             return True
         return False
+
+    def details(self):
+        if self.status is True and self.html is not None:
+            return {'title': (self.html.xpath('//*[@id="productTitle"]/text()') or [''])[0].strip(),
+                    'category': ([' '.join(x.strip() for x in  self.html.xpath('//*[@id="wayfinding-breadcrumbs_feature_div"]//li//text()'))] or [''])[0].strip(),
+                    'price': {'current':  (self.html.xpath('//*[@id="priceblock_ourprice"]/text()') or [''])[0].strip(),
+                              'original': (self.html.xpath('//*[text()= "List Price:"]/following-sibling::td/span[1]/text()') or [''])[0].strip(),
+                              'saved':    (self.html.xpath('//*[@id="regularprice_savings"]/td[2]/text()') or [''])[0].strip()},
+                    'rating': (self.html.xpath('//*[@id="averageCustomerReviews"]/span[1]/span/@title') or [''])[0].strip(),
+                    'availability': (self.html.xpath('//*[@id="availability"]/span/text()') or [''])[0].strip()}
+
